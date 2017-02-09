8f515915fa4c5adeeddc19f1fd83b63a3ac6f71a
Kit Kleckner <kit@kitatdotnet.net>
2017-02-08 20:47:45 -0800

    fixed album images url

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,18 +1,7 @@
 module AlbumsHelper
 
   def albumart_url(album,size = 'medium')
-    artist_name = album.album_artist_name
-    if artist_name
-      album_name = album.name
-      # "http://albumart-server.kitatdot.net/#{artist_name}/#{album_name}/cover/#{size}" 
-    else
-      # "/assets/64x64/devices/media-optical-audio_mount.png"
-      artist_name = '_DEFAULT_'
-      album_name = '_DEFAULT_'
-    end
-
-    "http://media.kitatdot.net/albumart/#{URI.encode(artist_name)}/#{URI.encode(album_name)}/cover/#{size}" 
+    "/albums/#{album.id}/image?sz=#{size}"
   end
-
 
 end
